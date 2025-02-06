27091bb4ac21a8e19e3947035e1708eb44b941a2
Acess0 <hubert.palczynski09@gmail.com>
2025-02-06 21:18:59 +0100

    Create user_setting.py

---
+++ main_scripts/user_setting.py
@@ -0,0 +1,29 @@
+# user_settings.py
+
+""" Constants used in other scripts are defined here. Modify them as you need before running the scripts. """
+
+
+# SETTINGS FOR:
+# 1_taking_pictures.py
+
+total_photos = 30  # Number of images to take and process thereafter
+countdown = 5  #Interval for count-down timer, seconds
+
+
+# SETTINGS FOR:
+# 2_image_selection.py
+# 3_calibration.py
+
+img_height = 480
+img_width = 640
+
+
+
+# SETTINGS FOR:
+# 3_calibration.py
+
+# Chessboard parameters
+# (Must use 6 Rows and 9 Column chessboard)
+rows = 6
+columns = 9
+square_size = 2.5
