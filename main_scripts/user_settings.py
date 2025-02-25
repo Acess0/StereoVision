02a96bffbb30aab232b46f708e25b32192e216ae
Acess0 <hubert.palczynski09@gmail.com>
2025-02-26 00:32:33 +0100

    Update user_settings.py

--- main_scripts/user_settings.py
+++ main_scripts/user_settings.py
@@ -1,29 +1,34 @@
 # user_settings.py
 
-""" Constants used in other scripts are defined here. Modify them as you need before running the scripts. """
+""" Stałe używane w innych programach"""
 
 
-# SETTINGS FOR:
+# Ustawienia dla:
 # 1_taking_pictures.py
 
-total_photos = 30  # Number of images to take and process thereafter
-countdown = 5  #Interval for count-down timer, seconds
+total_photos = 30  # Ilość zdjęć do zrobienia i przetworzenia
+countdown = 5  # Czas na wykonanie zdjęcia
 
 
-# SETTINGS FOR:
+# Ustawienia dla:
 # 2_image_selection.py
 # 3_calibration.py
-
-img_height = 360
+#dai.MonoCameraProperties.SensorResolution.THE_480_P)
+img_height = 460
 img_width = 640
+photo_height = 460
+photo_width = 1280
+#(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+#img_height = 400
+#img_width = 640
+#photo_height = 400
+#photo_width = 1280
 
 
-
-# SETTINGS FOR:
+# Ustawienia dla:
 # 3_calibration.py
 
-# Chessboard parameters
-# (Must use 6 Rows and 9 Column chessboard)
+# Parametry szachownicy
 rows = 6
 columns = 9
 square_size = 2.5
